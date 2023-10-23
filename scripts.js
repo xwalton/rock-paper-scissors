f739444210ac4135e894a428b89bdcc9fb43d09c
Xavier Walton <xavierwaltonpm@gmail.com>
2023-10-22 22:11:11 -0500

    created rock,paper,scissors logic
    made a function that returns a random choice for the computer.
    made a function that compares the choices and returns the winner.
    made a function that plays the game 5 times and returns the winner.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
@@ -8,6 +14,81 @@ function getComputerChoice() {
         case 2:
             return 'scissors'
     }
-    console.log(randomNumber);
 }
-Math.floor(Math.random() * (max - min + 1)) + min;
+
+function playRound() {
+    let playerSelection = prompt.toLowerCase("Do you choose rock, paper, or scissors?");
+    let playerScore = 0;
+    let computerScore = 0;
+    switch (playerSelection) {
+        case 'rock':
+            if (computerSelection === 'rock') {
+                computerScore++;
+                playerScore++;
+                return "It's a tie!";
+            } else if (computerSelection === 'paper') {
+                computerScore++;
+                return "You lose! Paper beats rock.";
+            } else {
+                playerScore++;
+                return "You win! Rock beats scissors.";
+            }
+        case 'paper':
+            if (computerSelection === 'paper') {
+                computerScore++;
+                playerScore++;
+                return "It's a tie!";
+            } else if (computerSelection === 'scissors') {
+                computerScore++;
+                return "You lose! Scissors beats paper.";
+            } else {
+                playerScore++;
+                return "You win! Paper beats rock.";
+            }
+        case 'scissors':
+            if (computerSelection === 'scissors') {
+                computerScore++;
+                playerScore++;
+                return "It's a tie!";
+            } else if (computerSelection === 'rock') {   
+                computerScore++;
+                return "You lose! Rock beats scissors.";
+            }   else {
+                playerScore++;
+                return "You win! Scissors beats paper.";
+            }
+        }
+    }
+
+function game() {
+    playRound();
+    console.log(playRound());
+    playRound();
+    console.log(playRound());
+    playRound();
+    console.log(playRound());
+    playRound();
+    console.log(playRound());
+    playRound();
+    console.log(playRound());
+    playRound();
+    console.log(playRound());
+    let computerScore = 0;
+    let playerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let result = playRound();
+        console.log(result);
+        if (result.includes("win")) {
+            playerScore++;
+        } else if (result.includes("lose")) {
+            computerScore++;
+        }
+    }
+    if (computerScore > playerScore) {
+        console.log("Computer wins!");
+    } else if (computerScore < playerScore) {
+        console.log("Player wins!");
+    } else {
+        console.log("It's a tie!");
+    }
+}
